4cb5c6ea09f9cb5e61dbe4c5c4173509d17291a3
Shanon Jackson <shanonjacksonnz@gmail.com>
2025-11-11 12:54:55 +1300

    Extend strip-runtime transform with stylesheet outputs

--- packages/native-transformers/compiled_babel/src/utils/traversers/index.rs
+++ packages/native-transformers/compiled_babel/src/utils/traversers/index.rs
@@ -1,1 +1,14 @@
-// TODO: port from utils/traversers/index.ts
+#[path = "get-export.rs"]
+pub mod get_export;
+pub mod object;
+pub mod set_imported_compiled_imports;
+pub mod types;
+
+#[allow(unused_imports)]
+pub use get_export::{get_default_export, get_named_export};
+#[allow(unused_imports)]
+pub use object::get_object_property_value;
+#[allow(unused_imports)]
+pub use set_imported_compiled_imports::set_imported_compiled_imports;
+#[allow(unused_imports)]
+pub use types::TraverserResult;
