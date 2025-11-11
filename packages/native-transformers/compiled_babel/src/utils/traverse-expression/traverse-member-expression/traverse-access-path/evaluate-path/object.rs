9ec3dbd7b45b0d7757db7d02723dc5cc39504db7
Shanon Jackson <shanonjacksonnz@gmail.com>
2025-11-11 13:22:50 +1300

    Handle jsxs calls in strip runtime

--- packages/native-transformers/compiled_babel/src/utils/traverse-expression/traverse-member-expression/traverse-access-path/evaluate-path/object.rs
+++ packages/native-transformers/compiled_babel/src/utils/traverse-expression/traverse-member-expression/traverse-access-path/evaluate-path/object.rs
@@ -1,1 +1,17 @@
-// TODO: port from utils/traverse-expression/traverse-member-expression/traverse-access-path/evaluate-path/object.ts
+use swc_core::ecma::ast::{Expr, ObjectLit};
+
+use crate::types::Metadata;
+use crate::utils_create_result_pair::{create_result_pair, ResultPair};
+use crate::utils_traversers_object::get_object_property_value;
+
+pub fn evaluate_object_path(
+    expression: &ObjectLit,
+    meta: Metadata,
+    property_name: &str,
+) -> ResultPair {
+    if let Some(result) = get_object_property_value(expression, property_name) {
+        return create_result_pair(result.node, meta);
+    }
+
+    create_result_pair(Expr::Object(expression.clone()), meta)
+}
